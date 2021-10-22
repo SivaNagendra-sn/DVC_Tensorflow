fa712084df92e5b3e6fb32d81cb0063845eb2dae
SivaNagendra-sn <sivanagendra.paruchuri@gmail.com>
2021-10-22 13:37:37 +0530

    Stage 2 created anf prepared Base model

--- src/stage_2_prepare_basemodel.py
+++ src/stage_2_prepare_basemodel.py
@@ -1,30 +1,58 @@
 from src.utils.all_utils import read_yaml, create_directory
+from src.utils.models import get_VGG_16_model, prepare_model
 import argparse
-import pandas as pd
 import os
-from tqdm import tqdm
 import logging
+import io
 
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
+                    filemode="a")
 
-
-def prepare_basemodel(config_path, params_path):
+def prepare_base_model(config_path, params_path):
     config = read_yaml(config_path)
     params = read_yaml(params_path)
 
-    # Reading all Artifacts paths from Config file
     artifacts = config["artifacts"]
     artifacts_dir = artifacts["ARTIFACTS_DIR"]
-    base_model_dir = artifacts["BASE_MODEL_DIRl_dir"]
+
+    base_model_dir = artifacts["BASE_MODEL_DIR"]
     base_model_name = artifacts["BASE_MODEL_NAME"]
 
     base_model_dir_path = os.path.join(artifacts_dir, base_model_dir)
 
-    # Creating directory for base model
     create_directory([base_model_dir_path])
 
     base_model_path = os.path.join(base_model_dir_path, base_model_name)
 
-    model = get_VGG_16_model(input_shape =params["IMAGE_SIZE"], model_path = base_model_path)
+    model = get_VGG_16_model(input_shape=params["IMAGE_SIZE"], model_path=base_model_path)
+
+    full_model = prepare_model(
+        model,
+        CLASSES=params["CLASSES"],
+        freeze_all=False,
+        freeze_till=2,
+        learning_rate=params["LEARNING_RATE"]
+    )
+
+    update_base_model_path = os.path.join(
+        base_model_dir_path,
+        artifacts["UPDATED_BASE_MODEL_NAME"]
+    )
+
+    def _log_model_summary(full_model):
+        with io.StringIO() as stream:
+            full_model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    logging.info(f"full model summary: \n{_log_model_summary(full_model)}")
+
+    full_model.save(update_base_model_path)
+    
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
@@ -35,9 +63,9 @@ if __name__ == '__main__':
     parsed_args = args.parse_args()
 
     try:
-        logging.info("\n >>>>> Stage-2-started")
-        prepare_basemodel(config_path=parsed_args.config, params_path=parsed_args.params)
-        logging.info("Stage 2 completed! Base model is now created")
+        logging.info(">>>>> stage two started")
+        prepare_base_model(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info("stage two completed! base model is created >>>>>")
     except Exception as e:
         logging.exception(e)
         raise e
